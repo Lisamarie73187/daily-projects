081f87e1420f31769861ce0ce4f242fab5a0c858
Lisa Herzberg <lisaherzberg@Lisas-MacBook-Pro.local>
2017-12-02 17:47:28 -0700

    friday added

--- notes.js
+++ notes.js
@@ -61,3 +61,30 @@ let cat1 = new Cat('maggie', 12, 'black', 'sad')
 console.log(cat1)
 console.log(cat1.sayMeow())
 
+
+var arr1 = [15,17,22,4,3,35,11,12,38,8];
+// Create a function that will average all the even or odd numbers in an array.
+function averageArray(arr,value){
+  var evens = []
+  var odds = []
+  for( i = 0; i < arr.length; i++){
+    if(value === 'evens' && arr[i]%2 === 0){
+      evens.push(arr[i])
+    }else if(value === 'odds' && arr[i]%2 !== 0){
+      odds.push(arr[i])
+    }
+  }return evens
+}
+
+
+console.log(averageArray(arr1,'evens'))
+ var a = averageArray(arr1,'evens')
+
+console.log(a)
+
+var total = a.reduce((total,element) => total + element)
+console.log(total)
+
+var str = '50000'
+var JI = str.split('')
+console.log(JI)
